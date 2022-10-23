4d35f2c44829448be57a34ff79f84c1be9534808
sebastiandixon <cyclewithsebastian@gmail.com>
2022-10-23 11:43:24 +0800

    Setup workspace, adding prettier settings and formatting repo

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,14 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    plugins: [require.resolve('prettier-plugin-tailwindcss')],
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
 };
